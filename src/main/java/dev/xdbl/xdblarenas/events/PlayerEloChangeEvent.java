3dc0ed893a13186979145eba84adf7717d99db23
Axodouble <53979495+Axodouble@users.noreply.github.com>
2023-11-11 11:18:14 +0100

    Added in full elo calculations, added in the scoreboard functionality and added in custom elo change event.

---
+++ src/main/java/dev/xdbl/xdblarenas/events/PlayerEloChangeEvent.java
@@ -0,0 +1,35 @@
+package dev.xdbl.xdblarenas.events;
+
+import dev.xdbl.xdblarenas.players.ArenaPlayer;
+import org.bukkit.entity.Player;
+import org.bukkit.event.Event;
+import org.bukkit.event.HandlerList;
+
+public class PlayerEloChangeEvent extends Event {
+
+    private static final HandlerList handlers = new HandlerList();
+    private final Player player;
+    private final ArenaPlayer arenaPlayer;
+
+    public PlayerEloChangeEvent(Player player, ArenaPlayer arenaPlayer) {
+        this.player = player;
+        this.arenaPlayer = arenaPlayer;
+    }
+
+    public Player getPlayer() {
+        return player;
+    }
+
+    public ArenaPlayer getArenaPlayer() {
+        return arenaPlayer;
+    }
+
+    @Override
+    public HandlerList getHandlers() {
+        return handlers;
+    }
+
+    public static HandlerList getHandlerList() {
+        return handlers;
+    }
+}
