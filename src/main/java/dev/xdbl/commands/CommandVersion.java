3af5afb16b76a1d5f1854a60a477d8c992a97fac
Axodouble <53979495+Axodouble@users.noreply.github.com>
2024-03-05 12:46:06 +0100

    Update CommandVersion.java

--- src/main/java/dev/xdbl/commands/CommandVersion.java
+++ src/main/java/dev/xdbl/commands/CommandVersion.java
@@ -21,8 +21,10 @@ public class CommandVersion implements CommandExecutor, TabCompleter {
 
     @Override
     public boolean onCommand(CommandSender sender, @NotNull Command cmd, @NotNull String label, String[] args) {
-        sender.sendMessage(MiniMessage.miniMessage().deserialize("<green>Running <white>Double <green>v" + plugin.getPluginMeta().getVersion() + " <green>by <white>Axodouble"));
-    return true;
+        sender.sendMessage(
+            MiniMessage.miniMessage().deserialize("<green>Running <white>Double <green>v" + plugin.getPluginMeta().getVersion() + " <green>by <white>Axodouble")
+        );
+        return true;
     }
 
     @Override
