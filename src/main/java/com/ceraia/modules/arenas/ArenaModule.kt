2c112676b2ab83ee7322dbfe501059548fe2e01c
Axodouble <git@axodouble.com>
2024-12-28 22:48:34 +0800

    Converted utils to kotlin

--- src/main/java/com/ceraia/modules/arenas/ArenaModule.kt
+++ src/main/java/com/ceraia/modules/arenas/ArenaModule.kt
@@ -4,8 +4,6 @@ import com.ceraia.Ceraia
 
 class ArenaModule(private val plugin: Ceraia) {
     init {
-        val arenaCommands = ArenaCommands(plugin)
-        plugin.getCommand("arena")?.setExecutor(arenaCommands::onCommand)
-        plugin.getCommand("arena")?.setTabCompleter(arenaCommands::onTabComplete)
+
     }
 }
--- src/main/java/com/ceraia/modules/arenas/Utils.kt
+++ src/main/java/com/ceraia/modules/arenas/Utils.kt
@@ -1,63 +1,65 @@
-package com.ceraia.modules.arenas;
+package com.ceraia.modules.arenas
 
-import com.ceraia.modules.arenas.types.Arena;
-import org.bukkit.Bukkit;
-import org.bukkit.Location;
-import org.bukkit.configuration.file.FileConfiguration;
-import org.bukkit.configuration.file.YamlConfiguration;
-import org.bukkit.entity.Player;
-import org.bukkit.inventory.ItemStack;
+import com.ceraia.modules.arenas.types.Arena
+import org.bukkit.Bukkit
+import org.bukkit.Location
+import org.bukkit.configuration.file.FileConfiguration
+import org.bukkit.configuration.file.YamlConfiguration
+import org.bukkit.entity.Player
+import org.bukkit.inventory.ItemStack
+import java.io.File
+import java.util.*
 
-import java.io.File;
-import java.util.Objects;
-
-public class Utils {
-
-    public static void revertInventory(Double plugin, Player pl, Arena arena) {
+object Utils {
+    @JvmStatic
+    fun revertInventory(plugin: Double, pl: Player, arena: Arena) {
         try {
-            File file = new File(plugin.getDataFolder(), "data/pinventory_" + arena.getName() + "_" + pl.getName() + ".yml");
+            val file = File(plugin.dataFolder, "data/pinventory_" + arena.name + "_" + pl.name + ".yml")
 
-            FileConfiguration config = YamlConfiguration.loadConfiguration(file);
+            val config: FileConfiguration = YamlConfiguration.loadConfiguration(file)
 
-            ItemStack[] content = new ItemStack[pl.getInventory().getContents().length];
+            val content = arrayOfNulls<ItemStack>(pl.inventory.contents.size)
             try {
-                for (String s : Objects.requireNonNull(config.getConfigurationSection("items")).getKeys(false)) {
-                    int i = Integer.parseInt(s);
-                    content[i] = config.getItemStack("items." + s);
+                for (s in Objects.requireNonNull(config.getConfigurationSection("items"))?.getKeys(false)!!) {
+                    val i = s.toInt()
+                    content[i] = config.getItemStack("items.$s")
                 }
-            } catch (Exception e) {
-                System.out.println("Problem loading player inventories.");
+            } catch (e: Exception) {
+                println("Problem loading player inventories.")
             }
 
-            pl.getInventory().setContents(content);
+            pl.inventory.contents = content
 
-            file.delete();
-        } catch (Exception e) {
-            e.printStackTrace();
-            System.out.println("Problem loading player inventory");
+            file.delete()
+        } catch (e: Exception) {
+            e.printStackTrace()
+            println("Problem loading player inventory")
         }
     }
 
-    public static void teleportPlayerToSpawn(Double plugin, Player player, Arena arena) {
-        String useLocation = plugin.getConfig().getString("spawn_teleport.use");
-        assert useLocation != null;
-        if (useLocation.equalsIgnoreCase("command")) {
-            Bukkit.dispatchCommand(
+    @JvmStatic
+    fun teleportPlayerToSpawn(plugin: Double, player: Player, arena: Arena) {
+        val useLocation = checkNotNull(plugin.config.getString("spawn_teleport.use"))
+        if (useLocation.equals("command", ignoreCase = true)) {
+            Objects.requireNonNull(plugin.config.getString("spawn_teleport.command"))?.let {
+                Bukkit.dispatchCommand(
                     Bukkit.getConsoleSender(),
-                    Objects.requireNonNull(plugin.getConfig().getString("spawn_teleport.command"))
-                            .replace("%player%", player.getName())
-            );
-        } else if (useLocation.equalsIgnoreCase("prior")) {
-            Location l = arena.getPlayerPriorLocation(player);
-            player.teleport(l);
+                    it
+                        .replace("%player%", player.name)
+                )
+            }
+        } else if (useLocation.equals("prior", ignoreCase = true)) {
+            val l = arena.getPlayerPriorLocation(player)
+            player.teleport(l)
         } else {
-            Location l = new Location(
-                    Bukkit.getWorld(Objects.requireNonNull(plugin.getConfig().getString("spawn_teleport.location.world"))),
-                    plugin.getConfig().getDouble("spawn_teleport.location.x"),
-                    plugin.getConfig().getDouble("spawn_teleport.location.y"),
-                    plugin.getConfig().getDouble("spawn_teleport.location.z")
-            );
-            player.teleport(l);
+            val l = Location(
+                Objects.requireNonNull(plugin.config.getString("spawn_teleport.location.world"))
+                    ?.let { Bukkit.getWorld(it) },
+                plugin.config.getDouble("spawn_teleport.location.x"),
+                plugin.config.getDouble("spawn_teleport.location.y"),
+                plugin.config.getDouble("spawn_teleport.location.z")
+            )
+            player.teleport(l)
         }
     }
 }
