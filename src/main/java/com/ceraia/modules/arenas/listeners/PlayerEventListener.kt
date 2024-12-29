ad5e8a226f9ffcb434141f623c7ecd9ffc3ca261
Axodouble <git@axodouble.com>
2024-12-29 20:00:55 +0800

    Fixed playereventlistener causing a crash

--- src/main/java/com/ceraia/modules/arenas/listeners/PlayerEventListener.kt
+++ src/main/java/com/ceraia/modules/arenas/listeners/PlayerEventListener.kt
@@ -1,16 +1,19 @@
 package com.ceraia.modules.arenas.listeners
 
-import com.ceraia.modules.ceraia.types.CeraiaPlayer
-import org.bukkit.entity.Player
 import org.bukkit.event.Event
 import org.bukkit.event.HandlerList
 
-class PlayerEventListener(val player: Player, val arenaPlayer: CeraiaPlayer) : Event() {
-    override fun getHandlers(): HandlerList {
-        return handlerList
+class PlayerEventListener : Event() {
+    companion object {
+        private val handlers = HandlerList()
+
+        @JvmStatic
+        fun getHandlerList(): HandlerList {
+            return handlers
+        }
     }
 
-    companion object {
-        val handlerList: HandlerList = HandlerList()
+    override fun getHandlers(): HandlerList {
+        return handlers
     }
 }
